8cc2c71aa8262a18940f8710b48d2f4709777f34
Stephane Letz <letz@grame.fr>
2020-06-13 15:48:48 +0200

    Correct pitchwheel handling. Set version to 1.0.19.

--- scripts/faustlive.js
+++ scripts/faustlive.js
@@ -538,7 +538,7 @@ window.addEventListener('touchstart', function () {
 
 // Main entry point, called when libfaust.js has finished to load
 function init() {
-  console.log('FaustEditor: version 1.0.18');
+  console.log('FaustEditor: version 1.0.19');
 
   // Try to load code from current URL
   configureEditorFromUrlParams();
