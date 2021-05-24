30a5bce54ca523d73c97a2d41a27bfc60123a68e
Stephane Letz <letz@grame.fr>
2021-05-24 16:55:24 +0200

    Correct syntax-highlighting.

--- codemirror/mode/faust/faust.js
+++ codemirror/mode/faust/faust.js
@@ -1,24 +1,24 @@
 // CodeMirror, copyright (c) by Marijn Haverbeke and others
 // Distributed under an MIT license: http://codemirror.net/LICENSE
 
-(function(mod) {
+(function (mod) {
     if (typeof exports == "object" && typeof module == "object") // CommonJS
         mod(require("../../lib/codemirror"), require("../clike/clike"));
     else if (typeof define == "function" && define.amd) // AMD
         define(["../../lib/codemirror", "../clike/clike"], mod);
     else // Plain browser env
         mod(CodeMirror);
-})(function(CodeMirror) {
+})(function (CodeMirror) {
     "use strict";
 
     var keywords =
-        "process component import library declare with environment waveform"
-        .split(" ");
+        "process component import library declare with environment route waveform soundfile"
+            .split(" ");
     var blockKeywords = "";
     var builtins = "";
     var atoms =
         "mem prefix int float rdtable rwtable select2 select3 ffunction fconstant fvariable button checkbox vslider hslider nentry vgroup hgroup tgroup vbargraph hbargraph attach acos asin atan atan2 cos sin tan exp log log10 pow sqrt abs min max fmod remainder floor ceil rint"
-        .split(" ");
+            .split(" ");
 
     function set(words) {
         var obj = {};
@@ -34,10 +34,10 @@
         builtin: set(builtins),
         atoms: set(atoms),
         hooks: {
-            "@": function(stream) {
+            "@": function (stream) {
                 return "meta";
             },
-            "'": function(stream) {
+            "'": function (stream) {
                 return "meta";
             }
         }
@@ -47,7 +47,7 @@
         atoms).concat(builtins));
 
     // This is needed to make loading through meta.js work.
-    CodeMirror.defineMode("faust", function(conf) {
+    CodeMirror.defineMode("faust", function (conf) {
         return CodeMirror.getMode(conf, "application/faust");
     }, "clike");
 });
