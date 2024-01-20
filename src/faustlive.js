fb9bc6ad470223087078403f9ac4a892324c97ec
Stephane Letz <letz@grame.fr>
2024-01-20 09:27:24 +0100

    Updated with faustwasm 0.0.53. Set version to 1.0.34.

--- src/faustlive.js
+++ src/faustlive.js
@@ -604,7 +604,7 @@ window.addEventListener('touchstart', function () {
 
 // Main entry point, called when libfaust.js has finished to load
 function init() {
-    console.log('FaustEditor: version 1.0.33');
+    console.log('FaustEditor: version 1.0.34');
 
     // Try to load code from current URL
     configureEditorFromUrlParams();
