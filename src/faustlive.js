9ba185bae90568de60aa0256232ff0b107a9e513
Stéphane Letz <letz@grame.fr>
2024-11-28 11:04:32 +0100

    Updated with faustwasm 0.7.6. Set version to 1.2.13.

--- src/faustlive.js
+++ src/faustlive.js
@@ -604,7 +604,7 @@ window.addEventListener('touchstart', function () {
 
 // Main entry point, called when libfaust.js has finished to load
 function init() {
-    console.log('FaustEditor: version 1.2.12');
+    console.log('FaustEditor: version 1.2.13');
 
     // Try to load code from current URL
     configureEditorFromUrlParams();
