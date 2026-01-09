063ce43c76cf493057f530bef7295b0ca26a989f
Stéphane Letz <letz@grame.fr>
2026-01-09 18:15:15 +0100

    Updated with faustwasm 0.15.0. Set version to 1.6.3.

--- src/faustlive.js
+++ src/faustlive.js
@@ -844,7 +844,7 @@ window.addEventListener('touchstart', function () {
  * Bootstraps the editor once the Faust WASM module is available.
  */
 function init() {
-    console.log('FaustEditor: version 1.6.2');
+    console.log('FaustEditor: version 1.6.3');
 
     // Try to load code from current URL
     configureEditorFromUrlParams();
